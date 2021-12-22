0af040cdfab12a428c751c95d2caa78b07b3a2d2
shubhamsingh941889 <36097959+shubhamsingh941889@users.noreply.github.com>
2021-12-22 19:16:36 +0530

    Add files via upload

---
+++ HMM/hmm.py
@@ -0,0 +1,220 @@
+
+from __future__ import print_function
+import json
+import numpy as np
+
+
+class HMM:
+
+    def __init__(self, pi, A, B, obs_dict, state_dict):
+        """
+        - pi: (1*num_state) A numpy array of initial probabilities. pi[i] = P(Z_1 = s_i)
+        - A: (num_state*num_state) A numpy array of transition probabilities. A[i, j] = P(Z_t = s_j|Z_{t-1} = s_i)
+        - B: (num_state*num_obs_symbol) A numpy array of observation probabilities. B[i, k] = P(X_t = o_k| Z_t = s_i)
+        - obs_dict: A dictionary mapping each observation symbol to its index 
+        - state_dict: A dictionary mapping each state to its index
+        """
+        self.pi = pi
+        self.A = A
+        self.B = B
+        self.obs_dict = obs_dict
+        self.state_dict = state_dict
+
+    def forward(self, Osequence):
+        """
+        Inputs:
+        - Osequence: (1*L) A numpy array of observation sequence with length L
+
+        Returns:
+        - alpha: (num_state*L) A numpy array where alpha[i, t-1] = P(Z_t = s_i, X_{1:t}=x_{1:t})
+                 (note that this is alpha[i, t-1] instead of alpha[i, t])
+        """
+        O = self.find_item(Osequence)
+        L = len(Osequence)
+        S = len(self.pi)
+        alpha = np.zeros([S, L])
+        ######################################################
+        # TODO: compute and return the forward messages alpha
+        ######################################################
+        my_op = self.obs_dict[Osequence[0]]
+        for i in range(S):
+            alpha[i][0] = self.pi[i] * self.B[i][my_op]
+        for t in range(1, L):
+            my_xt = self.obs_dict[Osequence[t]]
+            for s in range(S):
+                my_sig_alpha = 0
+                for my_sline in range(S):
+                    my_sig_alpha = my_sig_alpha + self.A[my_sline][s] * alpha[my_sline][t - 1]
+                alpha[s][t] = self.B[s][my_xt] * my_sig_alpha
+        return alpha
+
+
+        
+
+    def backward(self, Osequence):
+        """
+        Inputs:
+        - Osequence: (1*L) A numpy array of observation sequence with length L
+
+        Returns:
+        - beta: (num_state*L) A numpy array where beta[i, t-1] = P(X_{t+1:T}=x_{t+1:T} | Z_t = s_i)
+                    (note that this is beta[i, t-1] instead of beta[i, t])
+        """
+        S = len(self.pi)
+        L = len(Osequence)
+        O = self.find_item(Osequence)
+        beta = np.zeros([S, L])
+        #######################################################
+        # TODO: compute and return the backward messages beta
+        #######################################################
+        T = L-1
+        for s in range(S):
+            beta[s][T] = 1
+        for tr in range(0, T):
+            t = T - 1 - tr
+            for s in range(S):
+                my_sig_beta = 0
+                for my_sline in range(S):
+                    my_sig_beta = my_sig_beta + self.A[s][my_sline] * self.B[my_sline][self.obs_dict[Osequence[t + 1]]] * beta[my_sline][t + 1]
+                beta[s][t] = my_sig_beta
+        return beta
+
+
+    def sequence_prob(self, Osequence):
+        """
+        Inputs:
+        - Osequence: (1*L) A numpy array of observation sequence with length L
+
+        Returns:
+        - prob: A float number of P(X_{1:T}=x_{1:T})
+        """
+        
+        #####################################################
+        # TODO: compute and return prob = P(X_{1:T}=x_{1:T})
+        #   using the forward/backward messages
+        #####################################################
+        T = len(Osequence) - 1
+        alpha = self.forward(Osequence)
+        prob = 0
+        for s in range(len(self.pi)):
+            prob += alpha[s][T]
+        return prob
+
+
+    def posterior_prob(self, Osequence):
+        """
+        Inputs:
+        - Osequence: (1*L) A numpy array of observation sequence with length L
+
+        Returns:
+        - gamma: (num_state*L) A numpy array where gamma[i, t-1] = P(Z_t = s_i | X_{1:T}=x_{1:T})
+		           (note that this is gamma[i, t-1] instead of gamma[i, t])
+        """
+        ######################################################################
+        # TODO: compute and return gamma using the forward/backward messages
+        ######################################################################
+        beta = self.backward(Osequence)
+        alpha = self.forward(Osequence)
+        S = len(self.pi)
+        T = len(Osequence)
+        prob = np.zeros([S, T])
+        my_denom = 0
+        for s in range(S):
+            my_denom += alpha[s][T - 1]
+        for i in range(T):
+            for s in range(S):
+                my_numer = alpha[s][i] * beta[s][i]
+                prob[s][i] = my_numer / my_denom
+        return prob
+
+
+    
+    def likelihood_prob(self, Osequence):
+        """
+        Inputs:
+        - Osequence: (1*L) A numpy array of observation sequence with length L
+
+        Returns:
+        - prob: (num_state*num_state*(L-1)) A numpy array where prob[i, j, t-1] = 
+                    P(Z_t = s_i, Z_{t+1} = s_j | X_{1:T}=x_{1:T})
+        """
+        S = len(self.pi)
+        L = len(Osequence)
+        prob = np.zeros([S, S, L - 1])
+        #####################################################################
+        # TODO: compute and return prob using the forward/backward messages
+        #####################################################################
+        beta = self.backward(Osequence)
+        alpha = self.forward(Osequence)
+        my_denom = 0
+        for s in range(S):
+            my_denom += alpha[s][L - 1]
+        for i in range(L - 1):
+            for s in range(S):
+                for my_sline in range(S):
+                    prob[s, my_sline, i] = self.A[s, my_sline] * self.B[my_sline, self.obs_dict[Osequence[i + 1]]] * beta[my_sline, i + 1] * alpha[s, i] / my_denom
+        return prob
+
+
+    def viterbi(self, Osequence):
+        """
+        Inputs:
+        - Osequence: (1*L) A numpy array of observation sequence with length L
+
+        Returns:
+        - path: A List of the most likely hidden states (return actual states instead of their indices;
+                    you might find the given function self.find_key useful)
+        """
+        
+        ################################################################################
+        # TODO: implement the Viterbi algorithm and return the most likely state path
+        ################################################################################
+        
+        S = len(self.pi)
+        T = len(Osequence)
+        my_bg_delta = np.zeros([S, T], dtype="int")
+        my_delta = np.zeros([S, T])
+        my_xt = self.obs_dict[Osequence[0]]
+        for s in range(S):
+            my_delta[s][0] = self.pi[s] * self.B[s][my_xt]
+
+        for t in range(1, T):
+            my_xt = self.obs_dict[Osequence[t]]
+            for s in range(S):
+                max_a_smalldelta = -1
+                argmax_a_smalldelta = -1
+                for my_sline in range(S):
+                    delta_temp = self.A[my_sline][s] * my_delta[my_sline][t - 1]
+                    if delta_temp > max_a_smalldelta:
+                        max_a_smalldelta = delta_temp
+                        argmax_a_smalldelta = my_sline
+                my_delta[s][t] = self.B[s][my_xt] * max_a_smalldelta
+                my_bg_delta[s][t] = argmax_a_smalldelta
+        my_zmax = []
+        z = np.argmax(my_delta[:, T - 1])
+        my_zmax.append(z)
+        for t in range(T - 1, 0, -1):
+            z = my_bg_delta[z][t]
+            my_zmax.append(z)
+        my_zmax = my_zmax[::-1]
+        path = [0] * len(my_zmax)
+
+        for state, observation in self.state_dict.items():
+            for i in range(len(my_zmax)):
+                if observation == my_zmax[i]:
+                    path[i] = state
+        return path
+
+
+
+    #DO NOT MODIFY CODE BELOW
+    def find_key(self, obs_dict, idx):
+        for item in obs_dict:
+            if obs_dict[item] == idx:
+                return item
+
+    def find_item(self, Osequence):
+        O = []
+        for item in Osequence:
+            O.append(self.obs_dict[item])
+        return O
